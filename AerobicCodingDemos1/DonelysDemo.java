bb5cfefbe2c4771c0e536e986c27a9a69e010a29
Lee Angioletti <langio@outlook.com>
2019-08-19 09:38:07 -0400

    removed assemssments and aerobic exercises

--- AerobicCodingDemos1/DonelysDemo.java
+++
@@ -1,9 +0,0 @@
-public class DonelysDemo {
-    public static void checkAge(int age) {
-        assert age >= 21 : "The person is underage, and therefore can't consume alcohol";
-        System.out.println("The person is " + age + " years old and can consume alcohol");
-    }
-    public static void main(String[] args) {
-        checkAge(22);
-    }
-}
